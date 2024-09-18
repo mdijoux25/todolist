e941ea8702daa81ea70cf9b535257c551b302bbb
Mathieu Dijoux <61043@supinfo.com>
2024-09-18 16:29:53 -0400

    avancement

--- apps/app.js
+++ apps/app.js
@@ -17,16 +17,50 @@ app.use(express.static('public'));
 app.get('/', async(req,res) => {
   try {
     await connectToDatabase();
-    TaskModel.find().then((alltasks) =>{
-      res.status(200).send(alltasks)
-    });
-    
-  }catch (error){
+    const alltasks = await TaskModel.find(); // Fetch all tasks from the database
+    res.render('index', { todos: alltasks }); 
+  } catch (error) {
     console.error('DB connection error', error);
      res.status(500).json({ error: 'DB connection error' });
   }
 });
 
+app.post('/addtasks', async (req, res) => {
+  try {
+    const { name, title, description, priority } = req.body; 
+    const task = new TaskModel({ name, title, description, priority });
+  await task.save();
+  res.redirect('/')
+  } catch (error) {
+    console.error('Error creating task', error);
+    res.status(500).json({error: 'Error creating task.'});
+  }
+});
+
+app.post('/delete/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    await TaskModel.findByIdAndDelete(id);
+    console.log( id+' Task deleted successfully.');
+    res.redirect('/');
+  } catch (error) {
+    console.error('Error deleting task', error);
+     res.status(500).json({ error: 'Error deleting task.' });
+  }
+});
+
+app.post('/edit/:id', async (req, res) => {
+  try {
+    const { id } =req.params;
+    const { title, name, description, priority } = req.body;
+    await TaskModel.findByIdAndUpdate(id, { title, name, description, priority });
+    res.status(200).json({ message: +id+' Task updated successfully.' });
+    
+  } catch (error) {
+    console.error('Error editing task', error);
+    res.status(500).json({ error: 'Error editing task.' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Program:\t${package.name}`)
